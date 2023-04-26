5af6246ca39bb36d487350b1b6d1e89d67e49bf0
Sevrin94 <115183505+Sevrin94@users.noreply.github.com>
2023-04-26 20:33:56 +0200

    New customer

--- src/main/java/com/example/webshop/models/Customer.java
+++ src/main/java/com/example/webshop/models/Customer.java
@@ -1,4 +1,24 @@
 package com.example.webshop.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "customers")
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String socialSecurityNumber;
 }
