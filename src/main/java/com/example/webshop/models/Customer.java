9755a409a48da04ad0ea4015fea4d6961f175181
knottem <59744487+knottem@users.noreply.github.com>
2023-04-27 19:37:39 +0200

    removed some

--- src/main/java/com/example/webshop/models/Customer.java
+++ src/main/java/com/example/webshop/models/Customer.java
@@ -1,23 +1,3 @@
 package com.example.webshop.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Entity
-@NoArgsConstructor
-public class Customer {
-
-    @Id
-    @GeneratedValue
-    private long id;
-
-    private String name;
-
-    public Customer(String name) {
-        this.name = name;
-    }
-}
+public class Customer { }
--- src/main/java/com/example/webshop/repositories/CustomerRepository.java
+++ src/main/java/com/example/webshop/repositories/CustomerRepository.java
@@ -1,7 +1,4 @@
 package com.example.webshop.repositories;
 
-import com.example.webshop.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository  {
 }
