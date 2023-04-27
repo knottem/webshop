0cd7600deb316d5c41b978943613e220c8633fc9
Tompa123 <mrguy_93@hotmail.com>
2023-04-27 14:08:27 +0200

    Itemcontroller update
    updated itemcontroller

--- src/main/java/com/example/webshop/repositories/ItemRepository.java
+++ src/main/java/com/example/webshop/repositories/ItemRepository.java
@@ -3,5 +3,8 @@ package com.example.webshop.repositories;
 import com.example.webshop.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByName(String name);
 }
