525aebf4b39782737864d93a6f05194d9752637c
Tompa123 <mrguy_93@hotmail.com>
2023-04-28 15:13:56 +0200

    item/buy update
    update

--- src/main/java/com/example/webshop/controllers/ItemController.java
+++ src/main/java/com/example/webshop/controllers/ItemController.java
@@ -1,13 +1,20 @@
 package com.example.webshop.controllers;
 
+import com.example.webshop.models.BuyOrder;
+import com.example.webshop.models.Customer;
 import com.example.webshop.models.Item;
+import com.example.webshop.models.Order;
+import com.example.webshop.repositories.CustomerRepository;
 import com.example.webshop.repositories.ItemRepository;
+import com.example.webshop.repositories.OrderRepository;
+import org.hibernate.type.descriptor.java.ObjectJavaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,9 +24,13 @@ public class ItemController {
 
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
     private final ItemRepository itemRepository;
+    private final CustomerRepository customerRepository;
+    private final OrderRepository orderRepository;
 
-    public ItemController(ItemRepository itemRepository) {
+    public ItemController(ItemRepository itemRepository, CustomerRepository customerRepository, OrderRepository orderRepository) {
         this.itemRepository = itemRepository;
+        this.customerRepository = customerRepository;
+        this.orderRepository = orderRepository;
     }
 
     @GetMapping({"/items", "/items/"})
@@ -52,4 +63,27 @@ public class ItemController {
         }
     }
 
+    @PostMapping("/items/buy")
+    public ResponseEntity<Object> buyItem(@RequestBody BuyOrder buyOrder) {
+        Customer customer = customerRepository.findById(buyOrder.getCustomerId()).isPresent() ? customerRepository.findById(buyOrder.getCustomerId()).get() : null;
+        if (customer == null) {
+            return new ResponseEntity<>(Collections.singletonMap("error", "Customer not found"), HttpStatus.NOT_FOUND);
+        } else {
+            List <Item> itemList = new ArrayList<>();
+            List<Long> items = buyOrder.getItemIds();
+            for (Long item : items) {
+                Item itemFromDb = itemRepository.findById(item).isPresent() ? itemRepository.findById(item).get() : null;
+                if (itemFromDb == null) {
+                    return new ResponseEntity<>(Collections.singletonMap("error", "Product not found"), HttpStatus.NOT_FOUND);
+                } else {
+                    itemList.add(itemFromDb);
+                }
+            }
+            Order order = new Order(customer, itemList);
+            orderRepository.save(order);
+            logger.info("Order created: " + order);
+            return new ResponseEntity<>(order, HttpStatus.CREATED);
+        }
+
+    }
 }
